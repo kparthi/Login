156dd96e837c6776a23e82426e90a110001c6661
kparthi <37983008+kparthi@users.noreply.github.com>
2018-07-03 14:41:26 +0530

    Initial commit

---
+++ src/test/java/JenkinsDemoTest.java
@@ -0,0 +1,76 @@
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class JenkinsDemoTest {
+
+	 WebDriver driver;
+	
+	
+	@Test(dataProvider="getdata")
+	public void login(String username,String password) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\parthiban.k\\workspace\\Testing\\lib\\chromedriver.exe");
+	     WebDriver driver=new ChromeDriver();
+		File file = new File("C:\\Users\\parthiban.k\\workspace\\Login\\src\\test\\java\\jk.properties");
+		  
+		FileInputStream fileInput = null;
+		try {
+			fileInput = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Properties prop = new Properties();
+		
+		//load properties file
+		try {
+			prop.load(fileInput);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
+		
+		driver.get("http://localhost:8091/Login/");
+		driver.findElement(By.xpath("//input[@id='Uname']")).sendKeys(prop.getProperty(username));
+		driver.findElement(By.xpath("//input[@id='Pword']")).sendKeys(prop.getProperty(password));
+		driver.findElement(By.xpath("//button[@type='button']")).click();
+		String d=driver.getTitle();
+		System.out.println(d);
+		assertEquals(d,d);
+		
+		
+	}
+	
+	
+	
+	/*@AfterTest
+	public void closebrowser() throws InterruptedException
+	{
+		Thread.sleep(60000L);
+		driver.quit();
+	}*/
+	
+	@DataProvider
+	public Object[][] getdata() {
+		
+		Object[][] data=new Object[2][2];
+		data[0][0] = "username";
+	    data[0][1] = "password";
+		data[1][0] = "username1";
+		data[1][1] = "password1";
+		return data;
+		
+		
+	}
+}
